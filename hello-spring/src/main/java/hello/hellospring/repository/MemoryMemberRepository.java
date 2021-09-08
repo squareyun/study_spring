3565ec11cb8992abe58d12979caea3d9a6728d7b
squareyun <squareyun@gmail.com>
2021-09-08 23:26:05 +0900

    회원 리포지토리 테스트 케이스 작성

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -18,7 +18,7 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.ofNullable(member.get(id));
+        return Optional.ofNullable(store.get(id));
     }
 
     @Override
@@ -32,4 +32,9 @@ public class MemoryMemberRepository implements MemberRepository {
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore() {
+        store.clear();
+    }
+
 }
